f46aad56299915ca6e689966d0e8edf3fb285755
Bruno Austin <brunoaugustine2@gmail.com>
2022-11-22 08:31:17 +0100

    update database

--- src/config/API/api.config.js
+++ src/config/API/api.config.js
@@ -1,9 +1,9 @@
 //import API_LOCAL from './api-local';
 import API_PROD from "./api-prod"
-import API_LOCAL from "./api-local"
-const hostname = window.location.hostname
+// import API_LOCAL from "./api-local"
+// const hostname = window.location.hostname
 const port = window.location.port
-let isLocalApi = +port >= 5000
+// let isLocalApi = +port >= 5000
 
-export const API = API_LOCAL
+export const API = API_PROD
 // console.log("API", API, hostname, isLocalApi);

73ac1e79bbf44c4dbfdd032b16c0254b69de21b0
Bruno Austin <brunoaugustine2@gmail.com>
2022-11-28 14:51:44 +0100

    remove add button from admin

--- src/pages/transactions/Transactions.js
+++ src/pages/transactions/Transactions.js
@@ -226,7 +226,8 @@ const Transactions = () => {
             <div className='product'>
                 <div className='mb-3 d-flex justify-content-between align-items-center'>
                     <h2 className='m-0'>Transactions</h2>
-                    <button className='add_btn me-3' onClick={() => setShowspan(!showspan)}> <img src='../../assets/img/about/plus.png' className='me-2' />Add</button>
+                    {AuthStorage.getStorageData(STORAGEKEY.roles) === 'user' ? <button className='add_btn me-3' onClick={() => setShowspan(!showspan)}> <img src='../../assets/img/about/plus.png' className='me-2' />Add</button> : <></>}
+
                     {
                         showspan &&
                         <div className='add_content' style={{ right: "50px", top: "131px" }}>

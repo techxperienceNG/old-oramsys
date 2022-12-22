f18cbf93a619bb25799354a4bb8718c0c4c85a09
Bruno Austin <brunoaugustine2@gmail.com>
2022-12-22 09:14:55 +0100

    fix download and view termsheet, fix related parties

--- src/config/API/api-prod.js
+++ src/config/API/api-prod.js
@@ -1,5 +1,5 @@
 const protocol = 'http';
-const host = 'http://3.219.151.79/';
+const host = 'https://oramsys-backend.vercel.app/';
 // const host = 'http://192.168.1.52:5002/';
 // const port = '5002';
 // const trailUrl = 'api/v1';

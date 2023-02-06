f80c8b486a2a7b72cc94eb57f2eba35dba4f4c16
Bruno Austin <brunoaugustine2@gmail.com>
2023-02-06 22:28:11 +0100

    new files

--- src/redux/actions/companydataAction.js
+++
@@ -1,8 +0,0 @@
-import { COMPANY_DATA } from '../types';
-
-export const companydataAction = (value) => (dispatch) => {
-    dispatch({
-        type: COMPANY_DATA,
-        payload: value,
-    });
-};
--- src/redux/actions/loadingAction.js
+++
@@ -1,8 +0,0 @@
-import { IS_LOADING } from '../types';
-
-export const setIsLoading = (value) => (dispatch) => {
-    dispatch({
-        type: IS_LOADING,
-        payload: value,
-    });
-};

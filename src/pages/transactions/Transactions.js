8bfeb8d0403d4e493af321e1663a426d65dc69c1
Bruno Austin <brunoaugustine2@gmail.com>
2023-02-27 19:18:33 +0100

    fix term sheet

--- src/pages/transactions/Transactions.js
+++ src/pages/transactions/Transactions.js
@@ -151,9 +151,6 @@ const Transactions = () => {
     const formateCurrencyValue = (data) => {
         if (data) {
             let value = data.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            // let prefix = CurrencyOptions.find((ele) => ele.label === contractDetails?.currency)?.prefix
-            // let suffix = CurrencyOptions.find((ele) => ele.label === contractDetails?.currency)?.suffix
-            // return prefix ? (prefix + value) : suffix ? (value + suffix) : value
             return value
         } else {
             return data
